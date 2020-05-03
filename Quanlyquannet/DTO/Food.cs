7535c56d604eea7b5d1c18943d2123fe4553240d
tdgiang <tdgiangdev@gmail.com>
2020-05-03 01:08:10 -0700

    Tao khung project

--- Quanlyquannet/DTO/Food.cs
+++ Quanlyquannet/DTO/Food.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quanlyquannet.Controllers
+namespace Quanlyquannet.DTO
 {
-    class LoginCtrl
+    class Food
     {
-
     }
 }
